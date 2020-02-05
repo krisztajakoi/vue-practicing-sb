e3c8ce0b2a4376693294365bfbd3d752fb428922
Lamap <lenkei.akos@gmail.com>
2020-02-05 11:31:06 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-practicing-sb
-Created with CodeSandbox
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import { practice } from "./functions";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  created () {
+    practice(this)
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
