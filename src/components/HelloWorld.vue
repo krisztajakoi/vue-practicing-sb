7bd53bf470f64bf16cb8ce7c911f7b074043df83
lenkei.akos <lenkei.akos@virgo.hu>
2020-02-05 12:20:53 +0100

    test from local 3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,7 +18,7 @@
         >eslint</a>
       </li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>Essential Béla</h3>
     <ul>
       <li>
         <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
